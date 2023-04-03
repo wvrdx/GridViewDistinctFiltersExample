3cc67f90fd203679af6c264ecb0164420c99da1f
Eduard Baitsym <ebaitsym@vectorsoftwarecom.onmicrosoft.com>
2023-04-03 11:04:59 +0300

    Add project files.

---
+++ Item.cs
@@ -0,0 +1,10 @@
+﻿namespace WpfApp1
+{
+    public class Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Comment { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using AutoFixture;
+
+namespace WpfApp1
+{
+    public class MainViewModel
+    {
+        private readonly Fixture _fixture;
+
+        public MainViewModel()
+        {
+            _fixture = new Fixture();
+        }
+
+        public List<Item> Items => _fixture.Create<List<Item>>();
+    }
+}
